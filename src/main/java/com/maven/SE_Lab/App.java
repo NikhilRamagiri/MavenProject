47e3a7fd33db72d776b9ab7a955436cf05b52289
NikhilRamagiri <nikhilramagiri2@gmail.com>
2022-11-23 11:38:24 +0530

    Maven Java Project

---
+++ src/main/java/com/maven/SE_Lab/App.java
@@ -0,0 +1,13 @@
+package com.maven.SE_Lab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is SE lab internal 1" );
+    }
+}
---
+++ src/test/java/com/maven/SE_Lab/AppTest.java
@@ -0,0 +1,20 @@
+package com.maven.SE_Lab;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
